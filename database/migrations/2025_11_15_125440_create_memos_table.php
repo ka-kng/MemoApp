d7da3e960e11cb531763004a2cd985c55ff5fe77
ka-kng <ka.kengo0503@gmail.com>
2025-11-19 23:20:41 +0900

    ログイン時のメモ登録ページ作成

--- database/migrations/2025_11_15_125440_create_memos_table.php
+++ database/migrations/2025_11_15_125440_create_memos_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('memo', function (Blueprint $table) {
+        Schema::create('memos', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('title');
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('memo');
+        Schema::dropIfExists('memos');
     }
 };
--- resources/views/memo/create.blade.php
+++ resources/views/memo/create.blade.php
@@ -9,34 +9,31 @@
         <form action="{{ route('memo.store') }}" method="POST">
             @csrf
 
-            <div class="fixed inset-0 pt-32 pb-5 px-5 z-20">
-
-                <div class="space-y-5">
-                    <div>
-                        <input name="title" type="text"
-                            class="p-2 bg-stone-700 w-full h-15 rounded placeholder-white text-2xl" placeholder="タイトル">
-                        @error('title')
-                            <p class="text-red-400 text-xl">{{ $message }}</p> {{-- 追加：エラー表示 --}}
-                        @enderror
-                    </div>
-
-                    <div>
-                        <input name="subtitle" type="text"
-                            class="p-2 bg-stone-700 w-full h-15 rounded placeholder-white text-xl" placeholder="サブタイトル">
-                        @error('subtitle')
-                            <p class="text-red-400 text-xl">{{ $message }}</p> {{-- 追加：エラー表示 --}}
-                        @enderror
-                    </div>
-
-                    <div>
-                        <textarea name="content" rows="10" class="p-2 bg-stone-700 w-full rounded placeholder-white"
-                            placeholder="メモを入力してください"></textarea>
-                    </div>
+            <div class="space-y-4">
+                <div>
+                    <input name="title" type="text"
+                        class="p-2 bg-stone-700 w-full h-15 rounded placeholder-white text-2xl" placeholder="タイトル">
+                    @error('title')
+                        <p class="text-red-400 text-xl">{{ $message }}</p> {{-- 追加：エラー表示 --}}
+                    @enderror
                 </div>
 
-                <div class="flex justify-center mt-3">
-                    <x-button type="submit" label="登録" />
+                <div>
+                    <input name="subtitle" type="text"
+                        class="p-2 bg-stone-700 w-full h-15 rounded placeholder-white text-xl" placeholder="サブタイトル">
+                    @error('subtitle')
+                        <p class="text-red-400 text-xl">{{ $message }}</p> {{-- 追加：エラー表示 --}}
+                    @enderror
                 </div>
+
+                <div>
+                    <textarea name="content" rows="10" class="p-2 bg-stone-700 w-full rounded placeholder-white"
+                        placeholder="メモを入力してください"></textarea>
+                </div>
+            </div>
+
+            <div class="flex justify-center mt-3">
+                <x-button type="submit" label="登録" />
             </div>
         </form>
 
